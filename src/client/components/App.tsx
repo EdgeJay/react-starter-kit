86bf36c309ad1f8fc493f7889858cc1fc3d2b5a4
Wu Huijie <hjwu85@gmail.com>
2019-03-31 16:12:31 +0800

    moved global styles into themes index page

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -2,9 +2,8 @@ import React, { useState } from 'react';
 import { setConfig } from 'react-hot-loader';
 import { hot } from 'react-hot-loader/root';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
-import styledNormalize from 'styled-normalize';
-import { defaultTheme, media } from '../themes';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme, GlobalStyle } from '../themes';
 import Header from './base/Header';
 import Page from './base/Page';
 import PageContent from './base/PageContent';
@@ -13,61 +12,6 @@ import Contact from './pages/Contact';
 import Home from './pages/Home';
 
 setConfig({ logLevel: 'debug', ignoreSFC: false });
-
-const GlobalStyle = createGlobalStyle`
-  ${styledNormalize}
-
-  html {
-    font-size: ${props => props.theme.fontSize};
-    font-family: ${props => props.theme.fontFamily};
-    min-height: 100%;
-    box-sizing: border-box;
-  }
-
-  *,
-  *:before,
-  *:after {
-    box-sizing: inherit;
-  }
-
-  #mount {
-    height: 100vh;
-  }
-
-  h1 {
-    font-size: ${props => props.theme.typo.h1.fontSize.phone};
-  }
-
-  h2 {
-    font-size: ${props => props.theme.typo.h2.fontSize.phone};
-  }
-
-  h3 {
-    font-size: ${props => props.theme.typo.h3.fontSize.phone};
-  }
-
-  h4 {
-    font-size: ${props => props.theme.typo.h4.fontSize.phone};
-  }
-
-  body, p, button, label {
-    font-size: ${props => props.theme.typo.p.fontSize.phone};
-  }
-
-  ${media.desktop`
-    h1 {
-      font-size: ${props => props.theme.typo.h1.fontSize.desktop};
-    }
-
-    h2 {
-      font-size: ${props => props.theme.typo.h2.fontSize.desktop};
-    }
-
-    h3 {
-      font-size: ${props => props.theme.typo.h3.fontSize.desktop};
-    }
-  `};
-`;
 
 function App() {
   const [sideMenuOpened, setSideMenuOpened] = useState<boolean>(true);
--- src/client/themes/index.ts
+++ src/client/themes/index.ts
@@ -1,5 +1,8 @@
-import { css, DefaultTheme, ThemedCssFunction } from 'styled-components';
+import { createGlobalStyle, css, DefaultTheme, ThemedCssFunction } from 'styled-components';
+import styledNormalize from 'styled-normalize';
 import defaultTheme from './defaultTheme';
+
+export { defaultTheme };
 
 export const BASE_FONT_PIXEL_SIZE = 10;
 
@@ -23,4 +26,57 @@ export const media = (Object.keys(minScreenPixelWidths) as Array<
   {} as { [key in keyof typeof minScreenPixelWidths]: ThemedCssFunction<DefaultTheme> }
 );
 
-export { defaultTheme };
+export const GlobalStyle = createGlobalStyle`
+  ${styledNormalize}
+
+  html {
+    font-size: ${props => props.theme.fontSize};
+    font-family: ${props => props.theme.fontFamily};
+    min-height: 100%;
+    box-sizing: border-box;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  #mount {
+    height: 100vh;
+  }
+
+  h1 {
+    font-size: ${props => props.theme.typo.h1.fontSize.phone};
+  }
+
+  h2 {
+    font-size: ${props => props.theme.typo.h2.fontSize.phone};
+  }
+
+  h3 {
+    font-size: ${props => props.theme.typo.h3.fontSize.phone};
+  }
+
+  h4 {
+    font-size: ${props => props.theme.typo.h4.fontSize.phone};
+  }
+
+  body, p, button, label {
+    font-size: ${props => props.theme.typo.p.fontSize.phone};
+  }
+
+  ${media.desktop`
+    h1 {
+      font-size: ${props => props.theme.typo.h1.fontSize.desktop};
+    }
+
+    h2 {
+      font-size: ${props => props.theme.typo.h2.fontSize.desktop};
+    }
+
+    h3 {
+      font-size: ${props => props.theme.typo.h3.fontSize.desktop};
+    }
+  `};
+`;
