ab26da5137985ca9d68ab7950e18a40619234520
Wu Huijie <hjwu85@gmail.com>
2019-04-01 10:25:16 +0800

    renamed IAction to IActionObject for clearer name

--- src/client/reducers/IAction.ts
+++
@@ -1,3 +0,0 @@
-export default interface IAction {
-  type: string;
-}
--- src/client/reducers/app.ts
+++ src/client/reducers/app.ts
@@ -1,6 +1,14 @@
-import IAction from './IAction';
+import { LOCATION_CHANGE } from 'connected-react-router';
+import IActionObject from './IActionObject';
 
-const reducers: { [key: string]: any } = {};
+const reducers: { [key: string]: (state: {}, action: {}) => {} } = {
+  [LOCATION_CHANGE]: app => {
+    console.log('location change!'); // tslint:disable-line
+    return {
+      ...app,
+    };
+  },
+};
 
-export default (state = {}, action: IAction) =>
+export default (state = {}, action: IActionObject) =>
   reducers && reducers[action.type] ? reducers[action.type](state, action) : state;
